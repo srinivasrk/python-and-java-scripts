3893a4be7134d0146c15dcd726e03b041c002fc6
srinivasrk <srinivasrk92@gmail.com>
2016-03-21 15:22:10 +0530

    modified sensex code

--- sensex-custom.py
+++ sensex-custom.py
@@ -1,8 +1,28 @@
-from BeautifulSoup import BeautifulSoup
-from urllib2 import Request
+from bs4 import BeautifulSoup
+import urllib.request
+from urllib.request import urlopen
 from time import sleep
 from datetime import datetime
-from urllib2 import urlopen
-def display_page():
-    url="http://money.cnn.com/data/world_markets/asia/"
+import webbrowser
+import urllib.request as urllib2
+def get_news():
+    url="http://money.cnn.com/data/world_markets/asia"
+    req = urlopen(url)
+    page = req.read();
+    scraping = BeautifulSoup(page)
+    print(scraping);
+    scraping.find("div", { "class" : "cnnBody_Right" })['style'] = "display:none"
+    scraping.find("div",{"class":"cnnBody_Left wsodContent"})['style']="width:1024px;height:1024px"
+    scraping.find("div",{"id":"wsod_worldMarkets"})['style']="width:1024px;height:1024px"
+    scraping.find("iframe", {"src":"http://markets.money.cnn.com/worldmarkets/map.asp?region=Asia"})['style']="width:1024px;height:1024px"
+   # print("found ?" , scraping.find("div",{"class":"wsod_worldMarketsMapImage"}))
+    print(scraping);
+    with open("sensexdata.html","w") as f:
+        f.write(str(scraping));
     
+    
+
+print("--------Sensex value - Python WebScraping project-------")
+print("This sample project reads Sensex value from \"timesofindia.indiatimes.com/business\" Page and generates a output file too in your C:\output.txt")
+print ("")
+get_news();

b76598b3ad24a1a75475f3675a13e060f5bb3284
Srinivas R Kulkarni <srinivasrk92@gmail.com>
2016-03-21 23:32:14 +0530

    Story 1 2 and 3 completed
    Only to get data from NSE and display

--- sensex-custom.py
+++ sensex-custom.py
@@ -1,28 +1,48 @@
 from bs4 import BeautifulSoup
-import urllib.request
-from urllib.request import urlopen
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from urllib2 import Request
+from urllib2 import urlopen
 from time import sleep
+import time
 from datetime import datetime
 import webbrowser
-import urllib.request as urllib2
+
+driver = webdriver.Firefox()
+driver.get('file://' + "c:\sensexdata.html")
+
 def get_news():
     url="http://money.cnn.com/data/world_markets/asia"
     req = urlopen(url)
     page = req.read();
-    scraping = BeautifulSoup(page)
-    print(scraping);
-    scraping.find("div", { "class" : "cnnBody_Right" })['style'] = "display:none"
-    scraping.find("div",{"class":"cnnBody_Left wsodContent"})['style']="width:1024px;height:1024px"
-    scraping.find("div",{"id":"wsod_worldMarkets"})['style']="width:1024px;height:1024px"
-    scraping.find("iframe", {"src":"http://markets.money.cnn.com/worldmarkets/map.asp?region=Asia"})['style']="width:1024px;height:1024px"
+    scraping = BeautifulSoup(page,"html.parser")
+    #scraping.find("div", { "class" : "cnnBody_Right" })['style'] = "display:none"
+    #scraping.find("div",{"class":"cnnBody_Left wsodContent"})['style']="width:1024px;height:1024px"
+    #scraping.find("div",{"id":"wsod_worldMarkets"})['style']="width:1024px;height:1024px"
+    #scraping.find("iframe", {"src":"http://markets.money.cnn.com/worldmarkets/map.asp?region=Asia"})['style']="width:1024px;height:1024px"
    # print("found ?" , scraping.find("div",{"class":"wsod_worldMarketsMapImage"}))
-    print(scraping);
-    with open("sensexdata.html","w") as f:
+    for link in scraping.findAll('iframe'):
+            if(link['src'].startswith('http://markets.money.cnn.com/worldmarkets')):
+                sourcelink = link['src']
+    req = urlopen(sourcelink)
+    scraping = BeautifulSoup(req,"html.parser");
+    scraping.find("body",{"class":"wsodContent"})['style']="zoom:1.5"
+    scraping.find("body",{"class":"wsodContent"})['style']="-ms-zoom: 1.5;-webkit-zoom: 1.5;-moz-transform:  scale(1.9,1.9);transform-origin:0 0",
+    "-moz-transform-origin: left center;"
+   
+    #print(scraping);
+    with open("c:\sensexdata.html","w") as f:
         f.write(str(scraping));
+   # webbrowser.open('file://' + "c:\sensexdata.html")
+    driver.refresh()
     
     
 
 print("--------Sensex value - Python WebScraping project-------")
 print("This sample project reads Sensex value from \"timesofindia.indiatimes.com/business\" Page and generates a output file too in your C:\output.txt")
 print ("")
-get_news();
+while True:
+    get_news();
+    time.sleep(5);
+
+
